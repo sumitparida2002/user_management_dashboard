585e3c5b1fa118c4fb4af658c6ee62ec36df06ea
Sumit Parida <80693203+sumitparida2002@users.noreply.github.com>
2024-04-26 11:18:29 -0400

    Database Driven Table

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,17 +1,11 @@
-import { Payment, columns } from "./columns";
+import { db } from "@/lib/db";
+import { User, columns } from "./columns";
 import { DataTable } from "./data-table";
 
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ];
+async function getData(): Promise<User[]> {
+  const users = await db.user.findMany();
+
+  return users;
 }
 
 export default async function DemoPage() {
